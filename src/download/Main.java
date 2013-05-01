e3374bc8abdf485f700c83676dc5e0d413b45832
gamer01 <stefan-heid@web.de>
2013-05-01 13:04:28 +0200

    another test

--- src/download/Main.java
+++ src/download/Main.java
@@ -16,7 +16,10 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		try {
-						setRootDir();
+				
+			
+			
+			setRootDir();
 			setRelativePath();
 			DownloadFiles downloader = new DownloadFiles(modDateMapUri);
 			downloadLinA(downloader);
